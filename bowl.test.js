9c4d70183259114a8d516899af1362dc1e540620
Tom Rolfe <tomrolfe20@gmail.com>
2023-01-27 10:13:09 +0000

    starting fresh

--- bowl.test.js
+++ bowl.test.js
@@ -1,3 +1,16 @@
+// [[3,4],[3,4],[5,5],[3,4],[10],[10],[3,3], [2,2], [2,2], [2,2]]
+//    7     7    10    7     10   10    6      4       4     4
+//                               +10   +3
+//                     +3              +3  +3
+
+// [[10],[10],[10],[10],[10],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
+//  10   10   10   10   10    2     2     2      2     2    2
+//       +10 +10
+//            +10  +10
+//                 +10  +10
+//                      +10  +1
+//                            +1 +1
+
 const Bowl = require('./bowl');
 
 let game;
@@ -8,96 +21,9 @@ beforeEach(() => {
 
 describe('bowl', () => {
   it('bowls twice and calculates the points', () => {
-    game.bowl(5, 3);
-    expect(game.score()).toEqual(8);
+    game.frame();
+    game.bowl(3);
+    game.bowl(4);
+    expect(game.score()).toEqual(7);
   });
-
-//   it('bowls a spare one round and a 3,4 the next and calculates the points', () => {
-//     game.bowl(5, 5);
-//     game.bowl(3, 3);
-//     expect(game.score()).toEqual(19);
-//   });
-//   it('bowls a strike one round and a 3,4 the next and calculates the points', () => {
-//     game.bowl(10);
-//     game.bowl(3, 3);
-//     expect(game.score()).toEqual(22);
-//   });
-//   it('bowls a spare on frame 10 and a 1 the next and calculates the points', () => {
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(5, 5); //10th frame spare
-//     game.bowl(1);
-//     expect(game.score()).toEqual(29);
-//   });
-
-//   it('bowls a strike on frame 10 and a 10,10 the next and calculates the points', () => {
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(10, 0); //10th frame spare
-//     game.bowl(10, 10);
-//     expect(game.score()).toEqual(48);
-//   });
-
-//   it('bowls a 1,1 on frame 10 and tries to roll an 11th frame', () => {
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1);
-//     game.bowl(1, 1); //10th frame
-//     game.bowl(1, 1);
-//     expect(game.score()).toEqual(20);
-//   });
-//   it('Bowls gutter shots every game', () => {
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     game.bowl(0, 0);
-//     expect(game.score()).toEqual(0);
-//   });
-//   it('Bowls a perfect game', () => {
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(10, 10);
-//     expect(game.score()).toEqual(300);
-//   });
-//   it('Bowls 2 strikes in a row then 2,2', () => {
-//     game.bowl(10);
-//     game.bowl(10);
-//     game.bowl(2, 2);
-
-//     expect(game.score()).toEqual(40);
-//   });
 });
